cba9623350ec9aed3aef6262d97f5c2f3f7649bf
helena-hollander <helena.hollander@gmail.com>
2024-06-05 13:44:13 +0200

    p5 test onsdag

---
+++ Insallation p5 test/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+   
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+ 
+  </header>
+
+  <div>
+    
+
+  </div>
+
+  <RouterView />
+</template>
